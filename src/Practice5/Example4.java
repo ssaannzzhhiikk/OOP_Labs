23430617a20792e65a3509530361a9889b399e39
Sanzhar <omarkhanovsanzh@gmail.com>
2026-04-01 22:48:43 +0500

    Problem 4

---
+++ src/Practice5/Example4.java
@@ -0,0 +1,54 @@
+package Practice5;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+
+public class Example4 {
+    public static void main(String[] args) {
+        double leftOperand, result, rightOperand;
+        String leftString, operator, rightString;
+        StringTokenizer tokenizer;
+        Scanner in = new Scanner(System.in);
+
+        tokenizer = new StringTokenizer(in.nextLine(), "+-*/", true);
+
+        try {
+            leftString = tokenizer.nextToken();
+            operator = tokenizer.nextToken();
+            rightString = tokenizer.nextToken();
+
+            leftOperand = Double.parseDouble(leftString);
+            rightOperand = Double.parseDouble(rightString);
+
+            switch (operator) {
+                case "+":
+                    result = leftOperand + rightOperand;
+                    break;
+                case "-":
+                    result = leftOperand - rightOperand;
+                    break;
+                case "*":
+                    result = leftOperand * rightOperand;
+                    break;
+                case "/":
+                    if (rightOperand == 0.0) {
+                        System.out.println("Cannot divide by zero");
+                        return;
+                    }
+                    result = leftOperand / rightOperand;
+                    break;
+                default:
+                    System.out.println("Unknown operator: " + operator);
+                    return;
+            }
+
+            System.out.println("Result: " + result);
+        } catch (NoSuchElementException nsee) {
+            System.out.println("Invalid syntax");
+        } catch (NumberFormatException nfe) {
+            System.out.println("One or more operands is not a number");
+        }
+    }
+}
