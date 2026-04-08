95ae9fc9cc0e80dc396176355ecb858f14a7eac7
Sanzhar <omarkhanovsanzh@gmail.com>
2026-04-08 12:48:46 +0500

    Учфьзду 4

--- src/Practice5/Example4.java
+++ src/Practice5/Example4.java
@@ -1,25 +1,24 @@
 package Practice5;
 
-import java.util.*;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
-public class Example4
-{
-    public static void main(String[] args)
-    {
-        double                 leftOperand, result, rightOperand;
-        String                 leftString, operator, rightString;
-        StringTokenizer        tokenizer;
+public class Example4 {
+    public static void main(String[] args) {
+        double leftOperand, result, rightOperand;
+        String leftString, operator, rightString;
+        StringTokenizer tokenizer;
         Scanner in = new Scanner(System.in);
 
         tokenizer = new StringTokenizer(in.nextLine(), "+", true);
 
-        try
-        {
-            leftString   = tokenizer.nextToken();
-            operator     = tokenizer.nextToken();
-            rightString  = tokenizer.nextToken();
+        try {
+            leftString = tokenizer.nextToken();
+            operator = tokenizer.nextToken();
+            rightString = tokenizer.nextToken();
 
-            leftOperand  = Double.parseDouble(leftString);
+            leftOperand = Double.parseDouble(leftString);
             rightOperand = Double.parseDouble(rightString);
 
             if (operator.equals("+"))
@@ -28,13 +27,9 @@ public class Example4
                 result = 0.0;
 
             System.out.println("Result: " + result);
-        }
-        catch (NoSuchElementException nsee)
-        {
+        } catch (NoSuchElementException nsee) {
             System.out.println("Invalid syntax");
-        }
-        catch (NumberFormatException nfe)
-        {
+        } catch (NumberFormatException nfe) {
             System.out.println("One or more operands is not a number");
         }
 
